4a87ebabc69541182ef5aa9318f3652cc5b3b9d4
SaraTullini <sara.tullini@edu.unife.it>
2024-01-15 10:47:23 +0100

    modifica app

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'dimmi che funzioni ti prego!\nnon mi deludere!\n'
+    str="dimmi che funzioni ti prego!\n non mi deludere!\n"
+    return str
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
