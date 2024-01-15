2c31b6b9bcd413a69bf1e8c9303746aaed20c4cd
SaraTullini <sara.tullini@edu.unife.it>
2024-01-15 10:51:46 +0100

    correzione andata a capo

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    str="dimmi che funzioni ti prego!\n non mi deludere!\n"
-    return str
+    return "dimmi che funzioni ti prego!<br> non mi deludere!<br>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
