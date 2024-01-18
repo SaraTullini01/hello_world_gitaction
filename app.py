21641b1de37879ecbe91e34c2c9013c5a7c7351d
SaraTullini <sara.tullini@edu.unife.it>
2024-01-18 10:18:41 +0100

    caricamento cose

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return "dimmi che funzioni ti prego!<br> non mi deludere!<br>"
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
+
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(_name_)
 
 @app.route('/')
-def hello_world():
-    return "dimmi che funzioni ti prego!<br> non mi deludere!<br>"
+def index():
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return render_template('index.html', current_time=current_time)
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     app.run(host='0.0.0.0', port=5000)
