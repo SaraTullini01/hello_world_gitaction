617f9d33161c962902428047ca02556747304a2d
SaraTullini <sara.tullini@edu.unife.it>
2024-01-19 20:13:37 +0100

    prova nome commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     
-    return "<center><h1>dovresti funzionare!</h1><br><p>ciao</p></center>"
+    return "<center><h1>dovresti funzionare!</h1><br><p>Riciao</p></center>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
