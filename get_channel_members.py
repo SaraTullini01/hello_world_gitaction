ccd7c80d8b902f9174c51bdc0ac78a64f1b35dd4
SaraTullini <sara.tullini@edu.unife.it>
2024-01-20 01:05:12 +0100

    test

--- get_channel_members.py
+++ get_channel_members.py
@@ -3,19 +3,15 @@
 from telegram import Bot
 from telegram.error import TelegramError
 
-TELEGRAM_BOT_TOKEN = "IlTuoTokenDelBot"
-CHANNEL_ID = -1001234567890
+TELEGRAM_BOT_TOKEN = "6653294032:AAH3Bn8rEbPPNbL7IC6FfEahAXbZ4Q6EZZM"
+CHANNEL_ID = -1001993783434
 
 bot = Bot(TELEGRAM_BOT_TOKEN)
 
 def get_channel_members_ids(channel_id):
     try:
-        # Ottieni il conteggio dei membri del canale
-        member_count = bot.get_chat_members_count(channel_id)
-
-        # Ottieni gli ID dei membri iterativamente
-        member_ids = [bot.get_chat_member(channel_id, i).user.id for i in range(member_count)]
-
+        members = bot.get_chat_members(channel_id)
+        member_ids = [member.user.id for member in members]
         return member_ids
     except TelegramError as e:
         print(f"Errore durante il recupero degli iscritti del canale: {e}")
